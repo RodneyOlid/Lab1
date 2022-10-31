881c119dcfc9b76ee4d447fbe89b885d8d5f8eb0
RodneyOlid <117031324+RodneyOlid@users.noreply.github.com>
2022-10-31 10:23:58 -0400

    Add files via upload

---
+++ ReminderApp/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int x = 10;
+        int y = 21;
+        System.out.println(x + y);
+    }
+
+}
